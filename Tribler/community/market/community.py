80026395a5d6b3cc4d94ba6f6b43d18bc87b12ca
Martijn de Vos <mdmartijndevos@gmail.com>
2018-12-07 15:35:12 +0100

    Fixed Python 3 syntax errors in market

--- Tribler/community/market/community.py
+++ Tribler/community/market/community.py
@@ -583,7 +583,8 @@ class MarketCommunity(Community, BlockListener):
         # Create the tick
         tick = Tick.from_order(order)
 
-        def on_block_created(block):
+        def on_block_created(blocks):
+            block, _ = blocks
             self.trustchain.send_block(block, ttl=2)
             if self.is_matchmaker:
                 tick.block_hash = block.hash
@@ -594,7 +595,7 @@ class MarketCommunity(Community, BlockListener):
             self.logger.info("Ask created with asset pair %s", assets)
             return order
 
-        return self.create_new_tick_block(tick).addCallback(lambda (blk, _): on_block_created(blk))
+        return self.create_new_tick_block(tick).addCallback(on_block_created)
 
     def create_bid(self, assets, timeout):
         """
@@ -617,7 +618,8 @@ class MarketCommunity(Community, BlockListener):
         # Create the tick
         tick = Tick.from_order(order)
 
-        def on_block_created(block):
+        def on_block_created(blocks):
+            block, _ = blocks
             self.trustchain.send_block(block, ttl=2)
             if self.is_matchmaker:
                 tick.block_hash = block.hash
@@ -628,7 +630,7 @@ class MarketCommunity(Community, BlockListener):
             self.logger.info("Bid created with asset pair %s", assets)
             return order
 
-        return self.create_new_tick_block(tick).addCallback(lambda (blk, _): on_block_created(blk))
+        return self.create_new_tick_block(tick).addCallback(on_block_created)
 
     def received_block(self, block):
         """
@@ -714,7 +716,7 @@ class MarketCommunity(Community, BlockListener):
         }
         deferred = self.trustchain.sign_block(peer, peer.public_key.key_to_bin(),
                                               block_type='tx_init', transaction=tx_dict)
-        return addCallback(deferred, lambda (blk1, blk2): blk1)
+        return addCallback(deferred, lambda blocks: blocks[0])
 
     @synchronized
     def create_new_tx_payment_block(self, peer, payment):
@@ -734,7 +736,7 @@ class MarketCommunity(Community, BlockListener):
         }
         deferred = self.trustchain.sign_block(peer, peer.public_key.key_to_bin(),
                                               block_type='tx_payment', transaction=tx_dict)
-        return addCallback(deferred, lambda (blk1, blk2): blk1)
+        return addCallback(deferred, lambda blocks: blocks[0])
 
     @synchronized
     def create_new_tx_done_block(self, peer, ask_order_dict, bid_order_dict, transaction):
@@ -760,7 +762,7 @@ class MarketCommunity(Community, BlockListener):
         }
         deferred = self.trustchain.sign_block(peer, peer.public_key.key_to_bin(),
                                               block_type='tx_done', transaction=tx_dict)
-        return addCallback(deferred, lambda (blk1, blk2): blk1)
+        return addCallback(deferred, lambda blocks: blocks[0])
 
     def on_tick(self, tick):
         """
@@ -1003,7 +1005,7 @@ class MarketCommunity(Community, BlockListener):
 
             if order.verified:
                 return self.create_new_cancel_order_block(order)\
-                    .addCallback(lambda (blk, _): self.trustchain.send_block(blk, ttl=2))
+                    .addCallback(lambda blocks: self.trustchain.send_block(blocks[0], ttl=2))
 
         return succeed(None)
 
