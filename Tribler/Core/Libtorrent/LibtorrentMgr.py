fc1de8750ba45de02977059e0fc13936375977bc
Martijn de Vos <mdmartijndevos@gmail.com>
2019-03-15 21:08:59 +0100

    BEP33 experiment

--- Tribler/Core/Libtorrent/LibtorrentMgr.py
+++ Tribler/Core/Libtorrent/LibtorrentMgr.py
@@ -99,8 +99,8 @@ class LibtorrentMgr(TaskManager):
         self.ltsession_metainfo = self.create_session(hops=0, store_listen_port=False)
 
         if has_bep33_support():
-            self.ltsession_metainfo.set_alert_mask(self.default_alert_mask | lt.alert.category_t.dht_log_notification)
-            self.dht_health_manager = DHTHealthManager(self.ltsession_metainfo)
+            self.get_session(0).set_alert_mask(self.default_alert_mask | lt.alert.category_t.dht_log_notification)
+            self.dht_health_manager = DHTHealthManager(self.get_session(0))
 
         # make temporary directory for metadata collecting through DHT
         self.metadata_tmpdir = tempfile.mkdtemp(suffix=u'tribler_metainfo_tmpdir')
@@ -602,22 +602,18 @@ class LibtorrentMgr(TaskManager):
         for hops, ltsession in self.ltsessions.items():
             if ltsession:
                 for alert in ltsession.pop_alerts():
-                    self.process_alert(alert, hops=hops)
+                    if isinstance(alert, lt.metadata_received_alert):
+                        self.got_metainfo(str(alert.handle.info_hash()))
+                    elif alert.__class__.__name__ == "dht_pkt_alert":
+                        # We received a raw DHT message - decode it and check whether it is a BEP33 message.
+                        decoded = bdecode(alert.pkt_buf)
+                        if 'r' in decoded:
+                            if 'BFsd' in decoded['r'] and 'BFpe' in decoded['r']:
+                                self.dht_health_manager.received_bloomfilters(decoded['r']['id'],
+                                                                              bytearray(decoded['r']['BFsd']),
+                                                                              bytearray(decoded['r']['BFpe']))
 
-        # We have a separate session for metainfo requests.
-        # For this session we are only interested in the metadata_received_alert.
-        if self.ltsession_metainfo:
-            for alert in self.ltsession_metainfo.pop_alerts():
-                if isinstance(alert, lt.metadata_received_alert):
-                    self.got_metainfo(str(alert.handle.info_hash()))
-                elif alert.__class__.__name__ == "dht_pkt_alert":
-                    # We received a raw DHT message - decode it and check whether it is a BEP33 message.
-                    decoded = bdecode(alert.pkt_buf)
-                    if 'r' in decoded:
-                        if 'BFsd' in decoded['r'] and 'BFpe' in decoded['r']:
-                            self.dht_health_manager.received_bloomfilters(decoded['r']['id'],
-                                                                          bytearray(decoded['r']['BFsd']),
-                                                                          bytearray(decoded['r']['BFpe']))
+                    self.process_alert(alert, hops=hops)
 
     def _check_reachability(self):
         if self.get_session() and self.get_session().status().has_incoming_connections:
