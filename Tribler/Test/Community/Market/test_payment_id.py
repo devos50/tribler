e58471597f58dc5c213866c85717abdf373488bb
Martijn de Vos <mdmartijndevos@gmail.com>
2019-03-24 17:22:39 +0100

    Made Payment ID binary type

--- Tribler/Test/Community/Market/test_payment_id.py
+++ Tribler/Test/Community/Market/test_payment_id.py
@@ -7,15 +7,8 @@ class PaymentIdTestSuite(unittest.TestCase):
     """Payment Id test cases."""
 
     def setUp(self):
-        self.payment_id1 = PaymentId("3")
-        self.payment_id2 = PaymentId("4")
-
-    def test_init(self):
-        """
-        Test the initialization of a quantity
-        """
-        with self.assertRaises(ValueError):
-            PaymentId(1)
+        self.payment_id1 = PaymentId(b"3")
+        self.payment_id2 = PaymentId(b"4")
 
     def test_str(self):
         """
@@ -27,13 +20,13 @@ class PaymentIdTestSuite(unittest.TestCase):
         """
         Test equality between payment ids
         """
-        self.assertEqual(self.payment_id1, PaymentId("3"))
+        self.assertEqual(self.payment_id1, PaymentId(b"3"))
         self.assertNotEqual(self.payment_id1, self.payment_id2)
-        self.assertEqual(NotImplemented, self.payment_id1.__eq__("3"))
+        self.assertEqual(NotImplemented, self.payment_id1.__eq__(b"3"))
 
     def test_hash(self):
         """
         Test the hash creation of a payment id
         """
-        self.assertEqual(self.payment_id1.__hash__(), "3".__hash__())
+        self.assertEqual(self.payment_id1.__hash__(), b"3".__hash__())
         self.assertNotEqual(self.payment_id1.__hash__(), self.payment_id2.__hash__())
--- Tribler/community/market/core/payment_id.py
+++ Tribler/community/market/core/payment_id.py
@@ -1,16 +1,18 @@
+from six import binary_type
+
+
 class PaymentId(object):
     """Used for having a validated instance of a payment id that we can monitor."""
 
     def __init__(self, payment_id):
         """
         :param payment_id: String representation of the id of the payment
-        :type payment_id: str
+        :type payment_id: binary_type
         :raises ValueError: Thrown when one of the arguments are invalid
         """
         super(PaymentId, self).__init__()
 
-        if not isinstance(payment_id, str):
-            raise ValueError("Payment id must be a string")
+        payment_id = payment_id if isinstance(payment_id, bytes) else binary_type(payment_id)
 
         self._payment_id = payment_id
 
@@ -23,6 +25,9 @@ class PaymentId(object):
 
     def __str__(self):
         return "%s" % self._payment_id
+
+    def __bytes__(self):
+        return self._payment_id
 
     def __eq__(self, other):
         if not isinstance(other, PaymentId):
