6310c838514ebd69533d8595cc918e49227b9c82
Martijn de Vos <mdmartijndevos@gmail.com>
2019-04-25 15:23:09 +0200

    Increased sync interval

--- Tribler/community/market/settings.py
+++ Tribler/community/market/settings.py
@@ -16,7 +16,7 @@ class MatchingSettings(object):
         self.fanout = 20
         self.match_window = 0  # How much time we wait before accepting a specific match
         self.match_send_interval = 0  # How long we should wait with sending a match message (to avoid overloading a peer)
-        self.sync_interval = 20  # Synchronization interval
+        self.sync_interval = 30  # Synchronization interval
 
         self.sync_policy = SYNC_POLICY_NONE
         self.dissemination_policy = DISSEMINATION_POLICY_NEIGHBOURS
